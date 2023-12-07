727b44c18d9ecc27ee2dfbd583d1dc3c3d099f4a
Manas Poddar <iamscientistmanas@gmail.com>
2023-12-07 20:30:14 +0530

    data stored to database

--- booksscraper/pipelines.py
+++ booksscraper/pipelines.py
@@ -5,59 +5,151 @@
 
 
 # useful for handling different item types with a single interface
+import mysql.connector
 from itemadapter import ItemAdapter
 
 
 class BooksscraperPipeline:
     def process_item(self, item, spider):
-        
+
         adapter = ItemAdapter(item)
 
-        ## Strip all the whitespaces for the strings
+        # Strip all the whitespaces for the strings
         field_names = adapter.field_names()
-        for field_name in field_names :
-            if field_name != 'description' :
+        for field_name in field_names:
+            if field_name != 'description':
                 value = adapter.get(field_name)
                 adapter[field_name] = value.strip()
 
-        ## Category & Product Types --> switch to lowercase
-        lowercase_keys = ['category','product_type']
-        for lowercase_key in lowercase_keys :
+        # Category & Product Types --> switch to lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
             value = adapter.get(lowercase_key)
             adapter[lowercase_key] = value.lower()
 
-        ## Price --> conversion and cleaning
+        # Price --> conversion and cleaning
         price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
-        for price_key in price_keys :
+        for price_key in price_keys:
             value = adapter.get(price_key)
             value = value.replace('£', '')
             adapter[price_key] = float(value)
 
-        ## Availability of books - only extract numbers
+        # Availability of books - only extract numbers
         availability_string = adapter.get('availability')
-        if 'In stock' in availability_string :
-            adapter['availability'] = int(availability_string.split('(')[1].split(' ')[0])
-        else :
+        if 'In stock' in availability_string:
+            adapter['availability'] = int(
+                availability_string.split('(')[1].split(' ')[0])
+        else:
             adapter['availability'] = int(0)
 
-        ## Reviews --> To integer
+        # Reviews --> To integer
         num_reviews_string = adapter.get('num_reviews')
         adapter['num_reviews'] = int(num_reviews_string)
 
-        ## Turn stars Rating into integers
+        # Turn stars Rating into integers
         stars_string = adapter.get('stars')
         stars_text_value = stars_string.lower()
-        if stars_text_value == 'zero' :
+        if stars_text_value == 'zero':
             adapter['stars'] = 0
-        elif stars_text_value == 'one' :
+        elif stars_text_value == 'one':
             adapter['stars'] = 1
-        elif stars_text_value == 'two' :
+        elif stars_text_value == 'two':
             adapter['stars'] = 2
-        elif stars_text_value == 'three' :
+        elif stars_text_value == 'three':
             adapter['stars'] = 3
-        elif stars_text_value == "four" :
+        elif stars_text_value == "four":
             adapter['stars'] = 4
-        elif stars_text_value == 'five' :
+        elif stars_text_value == 'five':
             adapter['stars'] = 5
 
         return item
+
+
+class SaveToMySQLPipeline:
+
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='root',
+            database='books',
+        )
+
+        # Create cursor used to execute commands
+        self.cursor = self.conn.cursor()
+
+        # create books table if not exists
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS books(
+            id int NOT NULL auto_increment,
+            url VARCHAR(255),
+            title text,
+            upc VARCHAR(255),
+            product_type VARCHAR(255),
+            price_excl_tax DECIMAL,
+            price_incl_tax DECIMAL,
+            tax DECIMAL,
+            price DECIMAL,
+            availability INTEGER,
+            num_reviews INTEGER,
+            stars INTEGER,
+            category VARCHAR(255),
+            description text,
+            PRIMARY KEY (id)
+            )
+        """)
+    def process_item(self, item, spider) :
+        
+        ## Define insert statement
+        self.cursor.execute(""" insert into books (
+                url,
+                title,           
+                upc,
+                product_type,
+                price_excl_tax,
+                price_incl_tax,
+                tax,
+                price,
+                availability,
+                num_reviews,
+                stars,
+                category,
+                description
+            ) values (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+            )""" , (
+            item['url'],
+            item['title'],
+            item['upc'],
+            item['product_type'],
+            item['price_excl_tax'],
+            item['price_incl_tax'],
+            item['tax'],
+            item['price'],
+            item['availability'],
+            item['num_reviews'],
+            item['stars'],
+            item['category'],
+            item['description']
+            )
+        )
+
+        ## Execute insert into the database
+        self.conn.commit()
+        return item
+    
+    def close_spider(self, spider) :
+        self.cursor.close()
+        self.conn.close()
--- booksscraper/settings.py
+++ booksscraper/settings.py
@@ -67,6 +67,7 @@ ROBOTSTXT_OBEY = False
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    "booksscraper.pipelines.BooksscraperPipeline": 300,
+   "booksscraper.pipelines.SaveToMySQLPipeline" : 400,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
